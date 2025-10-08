e91811dcdc3d807868f1dbe58e8deb22a97da23b
andyliu <justgps@gmail.com>
2025-10-08 18:40:59 +0800

    <feature>add server func

---
+++ router.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+   "fmt"
+   "net/http"
+   "github.com/asccclass/sherryserver"
+)
+
+func NewRouter(srv *SherryServer.Server, documentRoot string)(*http.ServeMux) {
+   router := http.NewServeMux()
+
+   // 靜態檔案服務
+   staticfileserver := SherryServer.StaticFileServer{documentRoot, "index.html"}
+   staticfileserver.AddRouter(router)
+
+   // API 路由
+   router.HandleFunc("/", indexHandler)
+   router.HandleFunc("/api/locations", locationsHandler)
+   router.HandleFunc("/api/routes", routesHandler)
+   router.HandleFunc("/api/location-photos", locationPhotosHandler)
+   router.HandleFunc("/api/days", daysHandler)
+   return router
+}
